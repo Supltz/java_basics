0ed9737be79211164b67c87540d78ee18f171d95
LTZ <q981750318@gmail.com>
2021-03-28 15:02:29 +0800

    delete

--- AnotherModule/src/hash.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class hash {
-    public static void main(String[] args){
-     String s="ok";
-     StringBuilder sb=new StringBuilder(s);
-
-    }
-}
--- src/PackageTest/Employee.java
+++
@@ -1,4 +0,0 @@
-package PackageTest;
-
-public class Employee {
-}
